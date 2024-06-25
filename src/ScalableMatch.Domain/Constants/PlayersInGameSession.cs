2cedd3405cbb7fe6e3d496b37fa27d3112def38e
Dmitrii.Volkov <dmitrii.volkov@topia.com>
2024-06-25 21:41:30 +0300

    Specify min and max number of players in a session

---
+++ src/ScalableMatch.Domain/Constants/PlayersInGameSession.cs
@@ -0,0 +1,9 @@
+﻿namespace ScalableMatch.Domain.Constants
+{
+    public static class PlayersInGameSession
+    {
+        public const int Minumum = 2;
+
+        public const int Maximum = 10;
+    }
+}
