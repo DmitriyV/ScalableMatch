ad0946d9deca6a3e84ef1fdf911fdfa0fd5a90f2
Dmitrii.Volkov <dmitrii.volkov@topia.com>
2024-06-25 19:37:54 +0300

    Add BaseEntity

---
+++ src/ScalableMatch.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ScalableMatch.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        // Using non-generic string types for simplicity
+        public required string Id { get; set; }
+    }
+}
