0d37d22ecfa8a088186cbcc7997cd06f411fe7a9
Dmitrii.Volkov <dmitrii.volkov@topia.com>
2024-06-25 21:32:13 +0300

    Add GameSessionState

--- src/ScalableMatch.Domain/Entities/GameSession.cs
+++ src/ScalableMatch.Domain/Entities/GameSession.cs
@@ -1,9 +1,13 @@
-﻿namespace ScalableMatch.Domain.Entities
+﻿using ScalableMatch.Domain.Enums;
+
+namespace ScalableMatch.Domain.Entities
 {
     public class GameSession
     {
         public List<Player> Players { get; set; } = [];
+
         public int MaxPlayers { get; set; }
-        public string Status { get; set; }
+
+        public GameSessionState Status { get; set; }
     }
 }
---
+++ src/ScalableMatch.Domain/Enums/GameSessionState.cs
@@ -0,0 +1,11 @@
+﻿namespace ScalableMatch.Domain.Enums
+{
+    public enum GameSessionState
+    {
+        Created = 0,
+        Playing = 1,
+        Completed = 2,
+        Cancelled = 3,
+        Failed = 4
+    }
+}
